4e6aea1924712f7c9f2eb12b1e331ea7d946c8af
kai_Ker <kai_Ker@buaa.edu.cn>
2023-09-17 15:00:45 +0800

    HW2: Eliminate the magic numbers in the `GameManager` and use a shorter class name for the literals

---
+++ src/Constants.java
@@ -0,0 +1,18 @@
+
+public class Constants {
+    public static final int ADD_ADVENTURER = 1;
+    public static final int OBTAIN_BOTTLE = 2;
+    public static final int DROP_BOTTLE = 3;
+    public static final int OBTAIN_EQUIPMENT = 4;
+    public static final int DROP_EQUIPMENT = 5;
+    public static final int STAR_UP_EQUIPMENT = 6;
+
+    public static final int OP_IDX_TYPE = 0;
+    public static final int OP_IDX_ADV_ID = 1;
+    public static final int OP_IDX_ADV_NAME = 2;
+    public static final int OP_IDX_OBJ_ID = 2;
+    public static final int OP_IDX_OBJ_NAME = 3;
+    public static final int OP_IDX_BOT_CAP = 4;
+    public static final int OP_IDX_EQU_STAR = 4;
+
+}
--- src/GameManager.java
+++ src/GameManager.java
@@ -9,41 +9,41 @@ public class GameManager {
     }
 
     public void update(ArrayList<String> input) {
-        int type = Integer.parseInt(input.get(0));
-        int advID = Integer.parseInt(input.get(1));
+        int type = Integer.parseInt(input.get(Constants.OP_IDX_TYPE));
+        int advID = Integer.parseInt(input.get(Constants.OP_IDX_ADV_ID));
 
         Adventurer adventurer = adventurers.get(advID);
         switch (type) {
-            case OperatorLiterals.ADD_ADVENTURER: {
-                adventurers.put(advID, new Adventurer(advID, input.get(2)));
+            case Constants.ADD_ADVENTURER: {
+                adventurers.put(advID, new Adventurer(advID, input.get(Constants.OP_IDX_ADV_NAME)));
                 break;
             }
-            case OperatorLiterals.OBTAIN_BOTTLE: {
+            case Constants.OBTAIN_BOTTLE: {
                 adventurer.obtainBottle(
-                        Integer.parseInt(input.get(2)),
-                        input.get(3),
-                        Integer.parseInt(input.get(4))
+                        Integer.parseInt(input.get(Constants.OP_IDX_OBJ_ID)),
+                        input.get(Constants.OP_IDX_OBJ_NAME),
+                        Integer.parseInt(input.get(Constants.OP_IDX_BOT_CAP))
                 );
                 break;
             }
-            case OperatorLiterals.DROP_BOTTLE: {
-                adventurer.dropBottle(Integer.parseInt(input.get(2)));
+            case Constants.DROP_BOTTLE: {
+                adventurer.dropBottle(Integer.parseInt(input.get(Constants.OP_IDX_OBJ_ID)));
                 break;
             }
-            case OperatorLiterals.OBTAIN_EQUIPMENT: {
+            case Constants.OBTAIN_EQUIPMENT: {
                 adventurer.obtainEquipment(
-                        Integer.parseInt(input.get(2)),
-                        input.get(3),
-                        Integer.parseInt(input.get(4))
+                        Integer.parseInt(input.get(Constants.OP_IDX_OBJ_ID)),
+                        input.get(Constants.OP_IDX_OBJ_NAME),
+                        Integer.parseInt(input.get(Constants.OP_IDX_EQU_STAR))
                 );
                 break;
             }
-            case OperatorLiterals.DROP_EQUIPMENT: {
-                adventurer.dropEquipment(Integer.parseInt(input.get(2)));
+            case Constants.DROP_EQUIPMENT: {
+                adventurer.dropEquipment(Integer.parseInt(input.get(Constants.OP_IDX_OBJ_ID)));
                 break;
             }
-            case OperatorLiterals.STAR_UP_EQUIPMENT: {
-                adventurer.enhanceEquipment(Integer.parseInt(input.get(2)));
+            case Constants.STAR_UP_EQUIPMENT: {
+                adventurer.enhanceEquipment(Integer.parseInt(input.get(Constants.OP_IDX_OBJ_ID)));
                 break;
             }
             default: {
