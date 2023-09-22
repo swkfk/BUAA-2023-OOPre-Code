2f8df129368ad282afa03d7521bb633656cc294c
kai_Ker <kai_Ker@buaa.edu.cn>
2023-09-22 22:38:00 +0800

    HW3: Add constants for new requirements

--- src/Constants.java
+++ src/Constants.java
@@ -6,6 +6,13 @@ public class Constants {
     public static final int OBTAIN_EQUIPMENT = 4;
     public static final int DROP_EQUIPMENT = 5;
     public static final int STAR_UP_EQUIPMENT = 6;
+    public static final int OBTAIN_FOOD = 7;
+    public static final int DROP_FOOD = 8;
+    public static final int FETCH_EQUIPMENT = 9;
+    public static final int FETCH_BOTTLE = 10;
+    public static final int FETCH_FOOD = 11;
+    public static final int USE_BOTTLE = 12;
+    public static final int USE_FOOD = 13;
 
     public static final int OP_IDX_TYPE = 0;
     public static final int OP_IDX_ADV_ID = 1;
@@ -14,5 +21,6 @@ public class Constants {
     public static final int OP_IDX_OBJ_NAME = 3;
     public static final int OP_IDX_BOT_CAP = 4;
     public static final int OP_IDX_EQU_STAR = 4;
+    public static final int OP_IDX_FOOD_ENERGY = 5;
 
 }
