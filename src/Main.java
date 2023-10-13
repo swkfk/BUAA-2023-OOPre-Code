adf4f32c36c6f7f30eb2c33683c4f5a6b91bf2b4
kai_Ker <kai_Ker@buaa.edu.cn>
2023-10-13 14:11:31 +0800

    HW4: Fix bugs of input in `Main.java`

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<ArrayList<String>> inputInfo = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
+        GameManager mgr = new GameManager();
+
         int n = Integer.parseInt(scanner.nextLine().trim());
         for (int i = 0; i < n; ++i) {
-            String nextLine = scanner.nextLine();
-            String[] strings = nextLine.trim().split(" +");
-            inputInfo.add(new ArrayList<>(Arrays.asList(strings)));
-            if (strings[0].contains("-")) {
-                --i;
-            }
-        }
-
-        GameManager mgr = new GameManager();
-        int restLogCount = 0;
-        for (ArrayList<String> info : inputInfo) {
-            if (restLogCount == 0) {
-                mgr.clearFightMode();
-                restLogCount += mgr.update(info);
-            } else {
-                mgr.dispatchLog(info.get(0));
-                restLogCount -= 1;
+            String[] strings = scanner.nextLine().trim().split(" +");
+            int requireLogCount = mgr.update(new ArrayList<>(Arrays.asList(strings)));
+            for (int j = 0; j < requireLogCount; ++j) {
+                mgr.dispatchLog(scanner.nextLine());
             }
         }
     }
